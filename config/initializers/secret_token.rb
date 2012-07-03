498b246a781900245fe8f703cc37c947c4ad2042
Niquieaa <nicole.arboleda@gmail.com>
2012-07-03 11:14:48 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectCoco::Application.config.secret_token = 'b11ca2681db32efa4e44af0602d6d2fce8545a05d2ab8c9a3ba55e2b4033bbb53c34fb2d0ff2994265c4a514da9f7aa3a338b0e687328f40a8918dc76d851d1c'
